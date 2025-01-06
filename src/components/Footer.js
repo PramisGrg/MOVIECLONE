c9550ecdc5ee08525438c66034ebda38eb278cd4
PramisGrg <gurungpramis361@gmail.com>
2025-01-06 23:38:01 +0545

    Initail commit in new Repo

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-5">
+      <p className="text-center">All rights reserved &copy; 2022 | Pramis</p>
+    </div>
+  );
+};
+
+export default Footer;
